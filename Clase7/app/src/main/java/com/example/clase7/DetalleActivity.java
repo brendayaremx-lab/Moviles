d911ac487562c8b853cd68b2710081ceb4040e10
brenda <brendayaremx@gmail.com>
2026-04-14 21:35:19 -0600

    Tarea clase 9

--- Clase7/app/src/main/java/com/example/clase7/DetalleActivity.java
+++ Clase7/app/src/main/java/com/example/clase7/DetalleActivity.java
@@ -1,4 +1,34 @@
 package com.example.clase7;
 
-public class DetalleActivity {
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import com.bumptech.glide.Glide;
+
+public class DetalleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle);
+
+        String name  = getIntent().getStringExtra("name");
+        String desc  = getIntent().getStringExtra("desc");
+        String photo = getIntent().getStringExtra("photo");
+        int atack    = getIntent().getIntExtra("atack", 0);
+        int def      = getIntent().getIntExtra("def", 0);
+
+        ImageView imageView = findViewById(R.id.detallePhoto);
+        TextView tvName     = findViewById(R.id.detalleName);
+        TextView tvDesc     = findViewById(R.id.detalleDesc);
+        TextView tvAtack    = findViewById(R.id.detalleAtack);
+        TextView tvDef      = findViewById(R.id.detalleDef);
+
+        tvName.setText(name);
+        tvDesc.setText(desc);
+        tvAtack.setText("ATK: " + atack);
+        tvDef.setText("DEF: " + def);
+        Glide.with(this).load(photo).into(imageView);
+    }
 }
